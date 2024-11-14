d8db6495d3cede9e4b9e55113d6b66f09bdffbdd
kabinh07 <kanchon@polygontech.xyz>
2024-11-14 12:25:49 +0600

    refined

--- annotated_video_check/yolo_format_converter.py
+++ annotated_video_check/yolo_format_converter.py
@@ -1,7 +1,4 @@
 import json
-import os
-from PIL import Image, ImageDraw
-from tqdm import tqdm
 import subprocess
 
 map_label = {
@@ -26,7 +23,7 @@ def normalize_bbox(x, y, width, height, original_width, original_height):
     height = height / 100.0 * original_height
     return x_center, y_center, width, height
 
-with open('new_dataset.json', 'r') as f:
+with open('raw_data/new_dataset.json', 'r') as f:
     annotations = json.load(f)
 
 data = []
@@ -44,35 +41,7 @@ for idx, ann in enumerate(annotations):
             frame['frame'] = f"{file.split('.')[0]}_{frame['frame']}.jpg"
             frame['label'] = label
             data.append(frame)
-            
-            # row['frame_name'] = f"{file.split('.')[0]}_{frame['frame']}.jpg"
-            # row['results'] = [{'x': frame['x'], 'y': frame['y'], 'width': frame['width'], 'height': frame['height'], 'label': label}]
-            # row['x'] = frame['x']
-            # row['y'] = frame['y']
-            # row['width'] = frame['width']
-            # row['height'] = frame['height']
-            # row['label'] = label
-            # data.append(row)
 
-with open('dataset.json', 'w', encoding='utf-8') as f:
+with open('raw_data/dataset.json', 'w', encoding='utf-8') as f:
     json.dump(data, f)
-
-# with open('dataset.json', 'r') as f:
-#     dataset = json.load(f)
-
-# images = os.listdir('images')
-
-# img = Image.open(f"all_images/{images[0]}")
-# org_w, org_h = img.size
-
-# for idx, item in tqdm(enumerate(dataset), total = len(dataset)):
-#     print(f'images/{item['frame_name']}')
-#     img = Image.open(f'images/{item['frame_name']}')
-#     print(img.size)
-#     x, y, width, height = normalize_bbox(item['x'], item['y'], item['width'], item['height'], org_w, org_h)
-#     draw = ImageDraw.Draw(img)
-#     draw.rectangle([x, y, width+x, height+y], outline = 'red', width = 2)
-#     img.save(f'lol_{idx}.png')
-#     if idx == 10:
-#         break
 
